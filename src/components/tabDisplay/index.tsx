052c971099967df69a4797513a4d03051ae2b84a
Isaiah Bullard <isaiah0812@yahoo.com>
2023-02-11 10:03:11 -0700

    mobile responsivness

--- src/components/tabDisplay/index.tsx
+++ src/components/tabDisplay/index.tsx
@@ -4,6 +4,7 @@ import MusicDisplay from "./musicDisplay";
 import YoutubeDisplay from "./youtubeDisplay";
 import './TabDisplay.css';
 import SoundcloudDisplay from "./soundcloudDisplay";
+import { useEffect, useState } from "react";
 
 type TabDisplayProps = {
   content: Content;
@@ -11,8 +12,12 @@ type TabDisplayProps = {
 }
 
 export default function TabDisplay({ content, changing }: TabDisplayProps) {
+  const [ music, setMusic ] = useState<boolean>(false);
+  useEffect(() => {
+    setMusic(isMusic(content));
+  }, [content])
   return (
-    <div className={`tab-display-canvas${changing ? ' changing' : ''}`}>
+    <div className={`tab-display-canvas${changing ? ' changing' : ''}${music ? ' music' : ''}`}>
       <div className="tab-display">
         {isVideo(content) ? (
             <YoutubeDisplay content={content} />
